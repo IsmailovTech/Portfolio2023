b57d6ca544cf6e10509c21b36111638d441654b4
Ibrohimbek <ismailovibrokhim98@gmail.com>
2023-04-05 22:41:49 +0500

    changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
         >
           <motion.img
             variants={slideIn("up", "tween", 0.5, 1.3)}
-            src="./person.png"
+            src="./person.webp"
             alt=""
           />
         </motion.div>
